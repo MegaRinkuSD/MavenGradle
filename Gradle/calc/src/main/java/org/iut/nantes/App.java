ef3f5866df2f826e2e5274abcf3f033507b948f4
MegaSD <MegaSD@10.130.198.28>
2018-10-29 00:40:49 -0600

    Uploaded whole gradle project

---
+++ Gradle/calc/src/main/java/org/iut/nantes/App.java
@@ -0,0 +1,9 @@
+package org.iut.nantes;
+
+
+public class App {
+    public static void main(String[] args) {
+        Calculette a = new Calculette();
+        System.out.println(a.getSum(5, 2));
+        }
+}
---
+++ Gradle/calc/src/main/java/org/iut/nantes/Calculette.java
@@ -0,0 +1,13 @@
+package org.iut.nantes;
+
+public class Calculette {
+
+	public final int getSum(final int i1, final int i2) {
+		int res = 0;
+
+		res = i1 + i2;
+
+		return res;
+	}
+
+}
